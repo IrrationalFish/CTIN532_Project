d27bde8a07b1a6546733d8b5467ca3da30bee91d
NaughtyFishRei <35935912+NaughtyFishRei@users.noreply.github.com>
2020-02-18 16:13:31 -0800

    20200218-1613

---
+++ MagicalObject/Door2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : Mechanism {
+
+    private Animator anim;
+
+    private void Start() {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    public override void TurnOff() {
+        anim.SetBool("character_nearby", false);
+    }
+
+    public override void TurnOn() {
+        anim.SetBool("character_nearby", true);
+    }
+
+}
