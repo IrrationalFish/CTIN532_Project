cbf3785e24694aa5aed81bb8b064c35d468ed48b
NaughtyFishRei <35935912+NaughtyFishRei@users.noreply.github.com>
2020-02-22 01:01:30 -0800

    20200222-0101

--- Environment/DeathZone.cs
+++ Environment/DeathZone.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour {
 
+    private void Start() {
+        
+    }
+
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")) {
             print("Enter water");
-            GameManager.player.GetComponent<Rigidbody>().freezeRotation = false;
+            GameManager.player.GetComponent<Movement2>().enabled = false;
+            GameManager.player.GetComponent<FirstPersonCam>().enabled = false;
+            GameManager.player.GetComponent<Transform>().Rotate(new Vector3(0, 0, 10));
+            GameManager.player.GetComponent<CharacterController>().enabled = false;
+            GameManager.player.GetComponent<Rigidbody>().useGravity = true;
+            GameManager.player.GetComponent<CapsuleCollider>().enabled = true;
+            GameManager.player.GetComponent<FirstPersonCam>().enabled = false;
+            /*GameManager.player.GetComponent<Rigidbody>().freezeRotation = false;
             GameManager.player.GetComponent<FirstPersonCam>().enabled = false;
             GameManager.player.GetComponent<Movement>().enabled = false;
             GameManager.player.GetComponent<Rigidbody>().useGravity = true;
-            GameManager.player.GetComponent<Transform>().Rotate(new Vector3(0, 0, 10));
+            GameManager.player.GetComponent<Transform>().Rotate(new Vector3(0, 0, 10));*/
             Invoke("Respawn", 3f);
         }
     }
--- Player/Movement2.cs
+++ Player/Movement2.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
-
+public class Movement2 : MonoBehaviour {
     public float moveSpeed;
     public float gravity;
     public float jumpForce;
 
     private Rigidbody rb;
+    private CharacterController cc;
+    private Vector3 moveDir = Vector3.zero;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
     }
 
     void Update() {
@@ -22,23 +24,35 @@ public class Movement : MonoBehaviour {
     void UpdateMovement() {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        float verSpeed = rb.velocity.y;
-        if(!OnGround()) {
-            verSpeed = verSpeed - gravity * Time.deltaTime;
-        } else {
+
+        if(cc.isGrounded) {
+            moveDir = new Vector3(hor * moveSpeed, 0, ver * moveSpeed );
+            moveDir = transform.TransformDirection(moveDir);
             if(Input.GetKeyDown(KeyCode.Space)) {
-                verSpeed = jumpForce;
+                moveDir.y = jumpForce;
             }
         }
-        Vector3 movement = new Vector3(hor * moveSpeed, verSpeed, ver * moveSpeed);
-        rb.velocity = transform.TransformDirection(movement);
-
+        moveDir.y = moveDir.y - gravity * Time.deltaTime;
+        Vector3 parentVelocity = Vector3.zero;
+        if(gameObject.transform.parent != null) {
+            GameObject parent = gameObject.transform.parent.gameObject;
+            if(parent.GetComponent<Rigidbody>() != null) {
+                parentVelocity = parent.GetComponent<Rigidbody>().velocity;
+                print("Parent V: " + parentVelocity);
+            }
+        }
+        cc.Move(moveDir * Time.deltaTime + parentVelocity);
     }
 
     bool OnGround() {
         Debug.DrawRay(transform.position, new Vector3(0, -1, 0), Color.red, 1.1f);
-        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), 1.1f)){
-            return true;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), out hit, 1.1f)) {
+            if(hit.collider.isTrigger) {
+                return false;
+            } else {
+                return true;
+            }
         } else {
             return false;
         }
@@ -47,11 +61,11 @@ public class Movement : MonoBehaviour {
     public void CheckParent() {
         RaycastHit hit;
         if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), out hit, 1.1f)) {
-            if(hit.collider.gameObject.CompareTag("FloatingBoard") && this.gameObject.transform.parent==null) {
+            if(hit.collider.gameObject.CompareTag("FloatingBoard") && this.gameObject.transform.parent == null) {
                 //stand on the float board, link
                 this.gameObject.transform.parent = hit.collider.gameObject.transform;
                 print("player on floating board");
-            }else if(!hit.collider.gameObject.CompareTag("FloatingBoard")) {
+            } else if(!hit.collider.gameObject.CompareTag("FloatingBoard")) {
                 //not stand on the float board
                 if(gameObject.transform.parent != null && !gameObject.transform.parent.CompareTag("Elevator")) {
                     //not transporting by elevator
